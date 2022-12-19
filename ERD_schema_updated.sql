d38c919ccea93ec074cba7c873fbf778a05a8d44
Emily Coxon <emilyecoxon@gmail.com>
2022-12-19 21:16:09 +0800

    completed ETL report

--- ERD_schema_updated.sql
+++ ERD_schema_updated.sql
@@ -1,4 +1,35 @@
-﻿CREATE TABLE "Final_Table" (
+﻿CREATE TABLE "Labour_Force" (
+    "Date" date   NOT NULL,
+    "Employed_Total_Persons_thousands" float(2)   NOT NULL,
+    "Employed_Total_Males_thousands" float(2)   NOT NULL,
+    "Employed_Total_Females_thousands" float(2)   NOT NULL,
+    "Employed_Full_Time_Persons_thousands" float(2)   NOT NULL,
+    "Employed_Full_Time_Males_thousands" float(2)   NOT NULL,
+    "Employed_Full_Time_Females_thousands" float(2)   NOT NULL,
+    CONSTRAINT "pk_Labour_Force" PRIMARY KEY (
+        "Date"
+     )
+);
+CREATE TABLE "Weekly_Earnings" (
+    "Date" date   NOT NULL,
+    "Male_Earnings_Full_Time_$" float(2)   NOT NULL,
+    "Male_Earnings_Total_$" float(2)   NOT NULL,
+    "Female_Earnings_Full_Time_$" float(2)   NOT NULL,
+    "Female_Earnings_Total_$" float(2)   NOT NULL,
+    "People_Earnings_Full_Time_$" float(2)   NOT NULL,
+    "People_Earnings_Total_$" float(2)   NOT NULL,
+    CONSTRAINT "pk_Weekly_Earnings" PRIMARY KEY (
+        "Date"
+     )
+);
+CREATE TABLE "Retail_Turnover" (
+    "Date" date   NOT NULL,
+    "Retail_Turnover_WA_$millions" float(2)   NOT NULL,
+    CONSTRAINT "pk_Retail_Turnover" PRIMARY KEY (
+        "Date"
+     )
+);
+CREATE TABLE "Final_Table" (
     "Date" date   NOT NULL,
     "Male_Earnings_Full_Time_$" float(2)   NOT NULL,
     "Male_Earnings_Total_$" float(2)   NOT NULL,
@@ -18,39 +49,10 @@
      )
 );
 
-CREATE TABLE "Labour_Force" (
-    "Date" date   NOT NULL,
-    "Employed_Total_Persons_thousands" float(2)   NOT NULL,
-    "Employed_Total_Males_thousands" float(2)   NOT NULL,
-    "Employed_Total_Females_thousands" float(2)   NOT NULL,
-    "Employed_Full_Time_Persons_thousands" float(2)   NOT NULL,
-    "Employed_Full_Time_Males_thousands" float(2)   NOT NULL,
-    "Employed_Full_Time_Females_thousands" float(2)   NOT NULL,
-    CONSTRAINT "pk_Labour_Force" PRIMARY KEY (
-        "Date"
-     )
-);
 
-CREATE TABLE "Retail_Turnover" (
-    "Date" date   NOT NULL,
-    "Retail_Turnover_WA_$millions" float(2)   NOT NULL,
-    CONSTRAINT "pk_Retail_Turnover" PRIMARY KEY (
-        "Date"
-     )
-);
 
-CREATE TABLE "Weekly_Earnings" (
-    "Date" date   NOT NULL,
-    "Male_Earnings_Full_Time_$" float(2)   NOT NULL,
-    "Male_Earnings_Total_$" float(2)   NOT NULL,
-    "Female_Earnings_Full_Time_$" float(2)   NOT NULL,
-    "Female_Earnings_Total_$" float(2)   NOT NULL,
-    "People_Earnings_Full_Time_$" float(2)   NOT NULL,
-    "People_Earnings_Total_$" float(2)   NOT NULL,
-    CONSTRAINT "pk_Weekly_Earnings" PRIMARY KEY (
-        "Date"
-     )
-);
+
+
 
 ALTER TABLE "Retail_Turnover" ADD CONSTRAINT "fk_Retail_Turnover_Date" FOREIGN KEY("Date")
 REFERENCES "Labour_Force" ("Date");
